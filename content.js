8c2741c1caab2b6f48a214a8c3dc6447bdf3fae2
Yuri Gorokhov <yurigorokhov@gmail.com>
2014-01-21 10:21:14 -0800

    Solidifying api stats view

--- content.js
+++ content.js
@@ -22,8 +22,24 @@
 _.mixin(_.string.exports());
 
 //--- Controllers ---
+var MainCtrl = function($scope) {
+    var reload = function() {
+        chrome.devtools.inspectedWindow.eval(
+            "JSON.stringify({stats: Deki.Stats, base: Deki.BaseHref, pageTitle: Deki.PageTitle})",
+            function(result, isException) {
+                $scope.$apply(function() {
+
+                    // Handle error, likely this is due to not being on a MT site
+                    if(!isException && result) {
+                        $scope.$broadcast('data', JSON.parse(result));
+                    }
+                });
+            });
+    };
+    setInterval(reload, 500);
+};
+
 var PhpStatsCtrl = function($scope, $routeParams) {
-    var cache = null;
     var getApiLink = function(baseHref, path) {
         var url = _(path).strLeft('?');
         return {
@@ -37,153 +53,162 @@ var PhpStatsCtrl = function($scope, $routeParams) {
     $scope.data =  {
         error: 'Waiting for statistics'
     };
-    var reload = function() {
-        chrome.devtools.inspectedWindow.eval(
-            "JSON.stringify({stats: Deki.Stats, base: Deki.BaseHref})",
-            function(result, isException) {
-                $scope.$apply(function() {
+    $scope.$on('data', function(event, result) {
+        $scope.data.error = null;
 
-                    // Handle error, likely this is due to not being on a MT site
-                    if(isException || !result) {
-                        $scope.data.error = 'Waiting for statistics';
-                        statsKey = null;
+        // Cache totals
+        $scope.data.appCacheTotals = {
+            cache: result.stats.stats.totals.cache,
+            api: result.stats.stats.totals.cache,
+            other: result.stats.stats.totals.cache,
+            elapsed: result.stats.stats.totals.cache
+        };
+
+        // Cache stats
+        $scope.data.cachingResults = _(result.stats.stats.requests.cache).map(function(r) {
+            return {
+                verb: r.verb,
+                time: r.time,
+                hit: r.hit,
+                link: getApiLink(result.base, r.path)
+            }
+        });
+
+        // API stats columns
+        _(result.stats.stats.requests.api).each(function(req, idx) {
+            req.cols = _(req.stats).chain().words(/[;]/).groupBy(function(stat) {
+                return _(stat).strLeftBack('-').trim();
+            }).value();
+        });
+        var uniqueCols = ['request-time', 'mysql-time', 'mysql'];
+        _(result.stats.stats.requests.api).chain().pluck('cols').each(function(col) {
+            _(col).chain().keys().each(function(key) {
+                uniqueCols.push(key);
+            }).value();
+        }).value();
+        $scope.data.uniqueApiCols = _(uniqueCols).chain().map(function(c) {
+            return c === 'mysql' ? 'mysql (queries)' : c;
+        }).uniq().value();
+
+        // API stats values
+        $scope.data.apiRequests = _(result.stats.stats.requests.api).map(function(r) {
+            return {
+                verb: r.verb,
+                time: r.time,
+                link: getApiLink(result.base, r.path),
+                remainingCols: _($scope.data.uniqueApiCols).map(function(col) {
+                    var statMap = { };
+                    var str = '';
+                    var tdClassReset = '';
+                    var tdClass = '';
+                    var resetText = '';
+                    _(r.cols[col]).chain().words(',').each(function(stat) {
+                        statMap[_(stat).chain().strLeft('=').strRightBack('-').value()] = _(stat).strRight('=');
+                    });
+                    var hit = _(statMap['hit']).toNumber(3) || null;
+                    var miss = _(statMap['miss']).toNumber(3) || null;
+                    var ratio = _(statMap['ratio']).toNumber(3) || null;
+                    var reset = _(statMap['reset']).toNumber(3) || null;
+                    if(!miss && !hit && !ratio && !reset) {
+                        str = _(r.cols[col]).strRightBack('=') || '';
                     } else {
-
-                        // Only update if data changed
-                        $scope.data.error = null;
-                        var hash = btoa(result);
-                        if(cache === hash) {
-                            return;
-                        }
-                        cache = hash;
-                        result = JSON.parse(result);
-
-                        // Cache totals
-                        $scope.data.appCacheTotals = {
-                            cache: result.stats.stats.totals.cache,
-                            api: result.stats.stats.totals.cache,
-                            other: result.stats.stats.totals.cache,
-                            elapsed: result.stats.stats.totals.cache
-                        };
-
-                        // Cache stats
-                        $scope.data.cachingResults = _(result.stats.stats.requests.cache).map(function(r) {
-                            return {
-                                verb: r.verb,
-                                time: r.time,
-                                hit: r.hit,
-                                link: getApiLink(result.base, r.path)
+                        if(ratio) {
+                            if(!miss && hit) {
+                                miss = hit / ratio - hit; 
+                            } else if(!hit && miss) {
+                                hit = (-ratio * miss) / (ratio - 1); 
                             }
-                        });
-
-                        // API stats columns
-                        _(result.stats.stats.requests.api).each(function(req, idx) {
-                            req.cols = _(req.stats).chain().words(/[;]/).groupBy(function(stat) {
-                                return _(stat).strLeftBack('-').trim();
-                            }).value();
-                        });
-                        var uniqueCols = ['request-time', 'mysql-time', 'mysql'];
-                        _(result.stats.stats.requests.api).chain().pluck('cols').each(function(col) {
-                            _(col).chain().keys().each(function(key) {
-                                uniqueCols.push(key);
-                            }).value();
-                        }).value();
-                        $scope.data.uniqueApiCols = _(uniqueCols).chain().map(function(c) {
-                            return c === 'mysql' ? 'mysql (queries)' : c;
-                        }).uniq().value();
-
-                        // API stats values
-                        $scope.data.apiRequests = _(result.stats.stats.requests.api).map(function(r) {
-                            return {
-                                verb: r.verb,
-                                time: r.time,
-                                link: getApiLink(result.base, r.path),
-                                remainingCols: _($scope.data.uniqueApiCols).map(function(col) {
-                                    var statMap = { };
-                                    var str = '';
-                                    var tdClassReset = '';
-                                    var tdClass = '';
-                                    var resetText = '';
-                                    _(r.cols[col]).chain().words(',').each(function(stat) {
-                                        statMap[_(stat).chain().strLeft('=').strRightBack('-').value()] = _(stat).strRight('=');
-                                    });
-                                    var hit = _(statMap['hit']).toNumber(3) || null;
-                                    var miss = _(statMap['miss']).toNumber(3) || null;
-                                    var ratio = _(statMap['ratio']).toNumber(3) || null;
-                                    var reset = _(statMap['reset']).toNumber(3) || null;
-                                    if(!miss && !hit && !ratio && !reset) {
-                                        str = _(r.cols[col]).strRightBack('=') || '';
-                                    } else {
-                                        if(ratio) {
-                                            if(!miss && hit) {
-                                                miss = hit / ratio - hit; 
-                                            } else if(!hit && miss) {
-                                                hit = (-ratio * miss) / (ratio - 1); 
-                                            }
-                                        } else {
-                                            if(hit && miss) {
-                                                ratio = hit / (hit + miss);
-                                            } else if(!hit && miss) {
-                                                hit = 0;
-                                                ratio = 0.0;
-                                            } else if(hit && !miss) {
-                                                miss = 0;
-                                                ratio = 1.0;
-                                            }
-                                        }
-                                        var tdClass='';
-                                        str += hit || '0';
-                                        str += ' / ' + miss;
-                                        if(ratio > 0.99) {
-                                            tdClass = 'good';
-                                        } else if(ratio < 0.5) {
-                                            tdClass = 'bad';
-                                        }
-                                        if(reset) {
-                                            tdClassReset = ' reset'
-                                            resetText = 'reset: ' + reset;
-                                        }
-                                    }
-                                    return { 
-                                        display: str, 
-                                        formattedRatio: _(ratio * 100).numberFormat(1) || '', 
-                                        ratioClass: tdClass, 
-                                        resetClass: tdClassReset,
-                                        resetText: resetText
-                                    };
-                                })
-                            };
-                        });
+                        } else {
+                            if(hit && miss) {
+                                ratio = hit / (hit + miss);
+                            } else if(!hit && miss) {
+                                hit = 0;
+                                ratio = 0.0;
+                            } else if(hit && !miss) {
+                                miss = 0;
+                                ratio = 1.0;
+                            }
+                        }
+                        var tdClass='';
+                        str += hit || '0';
+                        str += ' / ' + miss;
+                        if(ratio > 0.99) {
+                            tdClass = 'good';
+                        } else if(ratio < 0.5) {
+                            tdClass = 'bad';
+                        }
+                        if(reset) {
+                            tdClassReset = ' reset'
+                            resetText = 'reset: ' + reset;
+                        }
                     }
-                });
-            });
-    };
-    setInterval(reload, 500);
+                    return { 
+                        display: str, 
+                        formattedRatio: _(ratio * 100).numberFormat(1) || '', 
+                        ratioClass: tdClass, 
+                        resetClass: tdClassReset,
+                        resetText: resetText
+                    };
+                })
+            };
+        });
+    });
 };
 var ApiStatsCtrl = function($scope, $routeParams, $http) {
-    $scope.data = { 
-        error: 'Fetching API statistics ...'
+    $scope.rate = function(val, good, bad) {
+        return val <= good ? 'good' : (val >= bad ? 'bad' : 'avg');
     };
-    proxyXHR({ 
-        method: 'GET',
-        url: $routeParams.uri + '&dream.out.format=json', 
-        onComplete: function(status, data) {
-            $scope.$apply(function() {
-                if(status !== 200) {
-                    $scope.data.error = 'Failed to fetch API stats';
-                } else {
-                    data = JSON.parse(data);
-                    $scope.data.error = null;
-                    $scope.data.totals = {
-                        elapsed: data['@elapsed'],
-                        siteid: data['@id'],
-                        path: data['@path']
-                    };
-                    console.log(data);
-                }
-            });
-        }
+    $scope.data = { 
+        pageTitle: 'Unknown page'
+    };
+    $scope.$on('data', function(event, result) {
+        $scope.data.pageTitle = result.pageTitle;
     });
+    $scope.refresh = function(uri) {
+        $scope.data.uri = uri;
+        $scope.data.error = 'Loading ...';
+        proxyXHR({ 
+            method: 'GET',
+            url: $routeParams.uri + '&dream.out.format=json', 
+            onComplete: function(status, data) {
+                $scope.$apply(function() {
+                    if(status !== 200) {
+                        $scope.data.error = 'Failed to fetch API stats';
+                    } else {
+                        data = JSON.parse(data);
+                        $scope.data.error = null;
+                        $scope.data.totals = {
+                            elapsed: data['@elapsed'],
+                            siteid: data['@id'],
+                            path: data['@path']
+                        };
+                        $scope.data.db = _(makeArray(data['db-summary'].query)).chain().map(function(d) {
+                            return {
+                                name: d['@name'],
+                                elapsed: d['@elapsed'],
+                                average: d['@average'],
+                                max: d['@max'],
+                                count: d['@count']
+                            };
+                        }).sortBy(function(x) { return -x.elapsed; }).value();
+                        $scope.data.pages = _(makeArray(data['rendered-content'].page)).chain().map(function(p) {
+                            return {
+                                path: p['@path'],
+                                elapsed: p['@elapsed']
+                            };
+                        }).sortBy(function(x) { return -x.elapsed; }).value();
+                    }
+                });
+            }
+        });
+    };
+    $scope.refresh($routeParams.uri);
+};
+
+//--- Helper Functions ---
+//TODO: move to service
+var makeArray = function(val) {
+    return _(val).isArray() ? val : [val];
 };
 
 //--- Angular setup ---
@@ -200,3 +225,4 @@ angular.module('dekiChromeProfiler', ['ngRoute'])
             redirectTo: '/phpstats'
         });
     });
+
